b6997be0106ba3b8a9102958ac10ff67391e99a7
anil kumar thakur <anilkumarthakur60@gmail.com>
2021-02-20 01:51:18 +0545

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Product\StoreProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
+use Facade\FlareClient\Http\Response;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api')->except('index','show');
+    }
 
     public function index()
     {
@@ -17,8 +22,16 @@ class ProductController extends Controller
     }
 
 
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
+        $product= new Product();
+        $product->name=$request->name;
+        $product->detail=$request->description;
+        $product->discount=$request->discount;
+        $product->stock=$request->stock;
+        $product->price=$request->price;
+            $product->save();
+            return response(['data'=>new ProductResource($product)],201);
         //
     }
 
